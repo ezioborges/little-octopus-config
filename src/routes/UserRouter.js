e23a5fa8dd43ba4faf03416117e47e7e06fcd838
ezioborges <eziopborges@gmail.com>
2025-03-17 13:09:42 -0300

    implement method /GET to route /users

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,2 +1,13 @@
-import express from "";
+import express from "express";
 import mongoose from "mongoose";
+import "../model/UserSchema.js";
+
+const User = mongoose.model("users");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  return res.status(201).send({ message: "Aqui esta funcionando" });
+});
+
+export default router;
